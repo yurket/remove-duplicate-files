9cac8ac4d20630266a19ee9ab5fa9e518d6c0ad2
yurket <yurket2006@gmail.com>
2017-12-19 16:53:03 +0000

    Added function returning vector of file paths

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-fn get_files_in_dir(dir_path: &Path) -> io::Result<()> {
-    for entry in std::fs::read_dir(dir_path)? {
-        let path = entry?.path();
-        if path.is_dir() {
-            get_files_in_dir(&path);
-        }
-        println!("{}", path.display());
+fn get_files_in_dir_nonrecursively(dir_path: &Path) -> Vec<PathBuf> {
+    let mut paths: Vec<PathBuf> = vec![];
+
+    for entry in std::fs::read_dir(dir_path).expect("read_dir call failed") {
+        paths.push(entry.unwrap().path());
     }
-    Ok(())
+    paths
 }
-
 
 fn main() {
     let test_dir = Path::new(".");
-    get_files_in_dir(test_dir).expect(&format!("Can't get files from directory \"{}\"", test_dir.display()));
+
+    print!("{:?}", get_files_in_dir_nonrecursively(test_dir));
 }
