1c1892ba641009b4c26feccf88410d1a4bb30db6
yurket <yurket2006@gmail.com>
2017-12-20 00:06:01 +0000

    Added removing file duplicates

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use std::fs;
+use std::collections::HashSet;
 
-fn get_files_in_dir_nonrecursively(dir_path: &Path) -> Vec<PathBuf> {
-    let mut paths: Vec<PathBuf> = vec![];
+extern crate sha2;
+use sha2::{Sha256, Digest};
 
-    for entry in std::fs::read_dir(dir_path).expect("read_dir call failed") {
-        paths.push(entry.unwrap().path());
+fn remove_file_duplicates(dir_path: &Path) -> Result<u32, std::io::Error>{
+    let mut n_duplicates: u32 = 0;
+    let mut file_hashes = HashSet::new();
+
+    for entry in std::fs::read_dir(dir_path)? {
+        let path_buf = entry?.path();
+        if path_buf.is_dir() {
+            continue;
+        }
+        let mut file = fs::File::open(path_buf.as_path())?;
+        let hash = Sha256::digest_reader(&mut file)?;
+
+        if file_hashes.contains(& hash) {
+            std::fs::remove_file(path_buf.as_path())?;
+            n_duplicates += 1;
+        }
+        else {
+            file_hashes.insert(hash);
+        }
+        println!("{:x}\t{:?}", hash, path_buf.as_path());
     }
-    paths
+
+    Ok(n_duplicates)
 }
+
 
 fn main() {
     let test_dir = Path::new(".");
 
-    print!("{:?}", get_files_in_dir_nonrecursively(test_dir));
+    match remove_file_duplicates(test_dir) {
+        Ok(n_duplicates) => println!("removed {} duplicates", n_duplicates),
+        Err(e) => println!("Error: {}", e),
+    }
 }
